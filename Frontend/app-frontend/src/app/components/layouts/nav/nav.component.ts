7d5f36950c7f05088faffafb6f8666ebaefed64b
SiguaPaul <paulsigua020802@gmail.com>
2025-03-05 11:25:50 -0500

    modify: se modifico para los layouts

--- Frontend/app-frontend/src/app/components/layouts/nav/nav.component.ts
+++ Frontend/app-frontend/src/app/components/layouts/nav/nav.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: false,
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  currentPath: string = '';
 
   pages = [
-    { title: 'Inicio', path: '/' },
-    { title: 'Indicadores', path: '/indicators' },
-    { title: 'Sala en brote', path: '/outbreak-room' },
-    { title: 'Sala en Brote por día', path: '/outbreak-room-by-day'}
-  ]
+    { title: 'INICIO', path: '/' },
+    { title: 'INDICADORES', path: '/indicators' },
+    { title: 'SALA EN BROTE', path: '/outbreak-room' },
+    { title: 'SALA EN BROTE POR DÍA', path: '/outbreak-room-by-day'}
+  ];
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentPath = this.router.url;
+    });
+  }
 }
---
+++ Frontend/app-frontend/src/app/services/indicators.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IndicatorsService {
+
+  apiUrl = environment.apiUrl
+
+  constructor(private http: HttpClient) { }
+
+  get_Indicador_Bal() {
+    const url = this.http.get(this.apiUrl + '/')
+  }
+}
